24f72b2f9f5befa47fedd9d2fb9f17190db4253c
Tetsuo Kohyama <tetsuo.kohyama@gmail.com>
2020-07-30 14:55:13 +0900

    Update comment lines

--- turnover_pfr.r
+++ turnover_pfr.r
@@ -8,22 +8,22 @@ library(cowplot)
 # Functions ---------------------------
 # biomass estimation based on Niiyama et al. (2010)
 biomass <- function(dbh, component = "agb") {
-  h <- 1 / (1 / (1.61 * dbh) + 1 / 69.) # height in Pasoh 4 ha
-  w_s <- 0.036 * (dbh * dbh * h)^1.01 # trunk plus branch, DDH in cm^3
-  w_l <- 1 / (1 / (0.108 * w_s^0.75) + 1 / 105) # leaves
-  w_r <- 0.023 * dbh^2.59 # coarse root
+  h <- 1 / (1 / (1.61 * dbh) + 1 / 69.) # tree height (m) from dbh (cm)
+  w_s <- 0.036 * (dbh * dbh * h)^1.01 # trunk plus branch stem (kg in dry mass)
+  w_l <- 1 / (1 / (0.108 * w_s^0.75) + 1 / 105) # leaves (kg in dry mass)
+  w_r <- 0.023 * dbh^2.59 # coarse root (kg in dry mss)
   if (component == "agb") {
     return(w_s + w_l)
   } else if (component == "all") {
     return(w_s + w_l + w_r)
-  } else if (component == "s") {
+  } else if (component == "stem") {
     return(w_s)
-  } else if (component == "l") {
+  } else if (component == "leaf") {
     return(w_l)
-  } else if (component == "r") {
+  } else if (component == "root") {
     return(w_r)
   } else {
-    stop("Invalid 'component'. Expected one of: 'all', 'agb', 's', 'l', 'r'")
+    stop("Invalid 'component'. Expected one of: 'all', 'agb', 'stem', 'leaf', 'root'")
   }
 }
 
@@ -69,7 +69,7 @@ productivity <- function(dbh1, dbh2, w1, w2, t, dbh_min, mass_min, area) {
   Bw <- ifelse(BT != B0, (BT - B0) / log(BT / B0), B0)
   B <- Bw / area
 
-  # size distribution for initial population
+  # Standardized maximum tree mass for initial population
   W_max <- as.numeric(quantile(w1[ri != 1], 0.99)) # Mg
 
   # turnover rates
@@ -78,12 +78,12 @@ productivity <- function(dbh1, dbh2, w1, w2, t, dbh_min, mass_min, area) {
   p <- turnover(w2, si * w1, t)
   l <- turnover(w1, si * w1, t)
 
-  # productivity
+  # absolute productivity (Mg per ha per year)
   P <- p * B
   P_simple <- sum(((si + ri) * w2 - si * w1) / t)
   P_simple_Clark <- sum(si * (w2 - w1) / t + ri * (w2 - mass_min) / t)
   P_simple <- P_simple / area
-  P_simple_Clark <- P_simple_Clark / area # cf. Clark et al. (2000), not recommended
+  P_simple_Clark <- P_simple_Clark / area # cf. Clark et al. (2001, Ecol. Appl.)
 
   return(list(
     "B" = B,
@@ -142,6 +142,7 @@ df2 <- data_preparation(df2)
 
 # Estimate production rate ------------
 # for each Cd
+dbh_min <- 1.
 mass_min <- biomass(2) / 1000 # tree biomass in Mg at dbh = 2 cm
 area <- 50 # total area in ha
 
@@ -180,7 +181,7 @@ print(Ps1)
 print("Identity free")
 print(Ps2)
 
-# fit a linear regression model
+# fit the linear regression model
 fit1 <- lm(log(p) ~ log(B), data = filter(res1, Cd != "Others"))
 pval1 <- summary(fit1)$coefficients[2, 4]
 print("Observed")
@@ -188,7 +189,7 @@ print(summary(fit1))
 
 fit2 <- lm(log(p) ~ log(B), data = filter(res2, Cd != "Others"))
 pval2 <- summary(fit2)$coefficients[2, 4]
-print("Identity free")
+print("Identity-free")
 print(summary(fit2))
 
 # draw a plot
@@ -205,14 +206,22 @@ p1 <- ggplot(filter(res1, Cd != "Others"), aes(x = B, y = p)) +
   annotate("text", x = 10, y = .2, label = "rare species\ncombined", hjust = .5) +
   scale_x_log10(limits = c(.0005, 100), breaks = xbreaks, labels = xbreaks) +
   scale_y_log10(limits = c(.001, .5)) +
-  labs(title = "(a) observed")
+  labs(
+    title = "(a) observed",
+    x = parse(text = "Biomass*','~B~(Mg %.% ha^{-1})"),
+    y = parse(text = "Relative~productivity*','~p~(year^{-1})")
+  )
 
 p2 <- ggplot(filter(res2, Cd != "Others"), aes(x = B, y = p)) +
   geom_point(alpha = .5) +
   geom_point(data = filter(res2, Cd == "Others"), shape = 18, size = 3,  alpha = .5) +
   scale_x_log10(limits = c(.0005, 100), breaks = xbreaks, labels = xbreaks) +
   scale_y_log10(limits = c(.001, .5)) +
-  labs(title = "(b) identity free")
+  labs(
+    title = "(b) identity-free",
+    x = parse(text = "Biomass*','~B~(Mg %.% ha^{-1})"),
+    y = parse(text = "Relative~productivity*','~p~(year^{-1})")
+  )
 
 if (pval1 < .05) {
   p1 <- p1 + geom_smooth(method = "lm", formula = y ~ x)
